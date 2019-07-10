9cf4015dc01f78cb9bc5a943cb1185655163581b
Damjan Kalšan <damjankalsan@gmail.com>
2019-07-11 00:05:22 +0200

    Added training phase

--- paper_ssim_autoencoder/autoencoder_torch.py
+++ paper_ssim_autoencoder/autoencoder_torch.py
@@ -4,6 +4,7 @@ import torch
 from torch import nn
 from torch import optim
 from torch.utils.data import DataLoader
+from torch.autograd import Variable
 import torch.nn.functional as F
 
 import torchvision
@@ -14,12 +15,16 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+import pytorch_ssim
+
 # Parameters
 LEARNING_RATE = 2e-4
 WEIGHT_DECAY = 1e-5
 ENCODING_DIM = 100
 RELU_SLOPE = 0.2
-BATCH_SIZE = 16
+BATCH_SIZE = 32
+K = 11
+NBR_EPOCHS = 1000
 
 # Visualization helper
 def imshow(img):
@@ -27,6 +32,12 @@ def imshow(img):
 	npimg = img.numpy()
 	plt.imshow(np.transpose(npimg, (1,2,0)), cmap='gray', vmin=0, vmax=255)
 	plt.show()
+
+def denormalize(imgs):
+    imgs = imgs / 2 + 0.5
+    imgs = imgs.clamp(0, 1)
+    imgs = imgs.view(-1, 1, 128, 128)
+    return imgs	
 
 # Transforms 
 data_transforms = transforms.Compose([
@@ -104,6 +115,36 @@ model = Autoencoder()
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 model.to(device)
 
-criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+ssim_loss = pytorch_ssim.SSIM(window_size=K)
 
+# Train
+for epoch in range(NBR_EPOCHS):
+
+	running_loss = 0.0
+
+	for i, data in enumerate(train_dl, 0):
+
+		images, _ = data
+		images = Variable(images).to(device)
+		
+		# Forward pass
+		reconstructions = model(images)
+		loss = -ssim_loss(images, reconstructions)
+
+		# Backward pass
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+
+		# Log
+		running_loss += loss.item()
+
+		# Save visualizations to disk
+		if(epoch % 50 == 0):
+			original_and_reconstructed = torch.cat((images[:4], reconstructions[:4]), 0)
+			original_and_reconstructed = denormalize(original_and_reconstructed)
+			torchvision.utils.save_image(original_and_reconstructed.cpu(), './images/epoch_{}.png'.format(epoch), nrow=4)
+
+	# Log
+	print("[Epoch {0}/{1}], loss: {2}".format(epoch, NBR_EPOCHS, running_loss/np.ceil(len(train_dl)/BATCH_SIZE)))
